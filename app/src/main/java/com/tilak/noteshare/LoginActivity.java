7539bed438047436d13d9f82151ecfaff3ae8a79
jayvisariya93 <jayvisariya93@gmail.com>
2015-10-10 15:03:49 +0530

    junk removed

--- app/src/main/java/com/tilak/noteshare/LoginActivity.java
+++ app/src/main/java/com/tilak/noteshare/LoginActivity.java
@@ -1,68 +1,23 @@
 package com.tilak.noteshare;
 
-import android.app.ProgressDialog;
-import android.content.Context;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
+import android.view.View;
 
-public class LoginActivity extends FragmentActivity implements FragmentManager.OnBackStackChangedListener {
-
-    public static final String SOCIAL_NETWORK_TAG = "SocialIntegrationMain.SOCIAL_NETWORK_TAG";
-    private static ProgressDialog pd;
-    static Context context;
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-        context = this;
-        getSupportFragmentManager().addOnBackStackChangedListener(this);
-        //homeAsUpByBackStack();
+    }
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new LoginFragment())
-                    .commit();
-        }
+    public void main(View v){
+        Intent i = new Intent(getApplication(), MainActivity.class);
+        startActivity(i);
 
     }
 
-    @Override
-    public void onBackStackChanged() { homeAsUpByBackStack(); }
-
-    private void homeAsUpByBackStack() {
-        int backStackEntryCount = getSupportFragmentManager().getBackStackEntryCount();
-        if (backStackEntryCount > 0) {
-            getActionBar().setDisplayHomeAsUpEnabled(true);
-        } else {
-            getActionBar().setDisplayHomeAsUpEnabled(false);
-        }
-    }
-
-    protected static void showProgress(String message) {
-        pd = new ProgressDialog(context);
-        pd.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        pd.setMessage(message);
-        pd.setCancelable(false);
-        pd.setCanceledOnTouchOutside(false);
-        pd.show();
-    }
-
-    protected static void hideProgress() {
-        pd.dismiss();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        Fragment fragment = getSupportFragmentManager().findFragmentByTag(SOCIAL_NETWORK_TAG);
-        if (fragment != null) {
-            fragment.onActivityResult(requestCode, resultCode, data);
-        }
-    }
 }
