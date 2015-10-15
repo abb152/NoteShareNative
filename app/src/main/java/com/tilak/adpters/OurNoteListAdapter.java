a50a675a1a5c50de84264181c24d532efc8984c9
jayvisariya93 <jayvisariya93@gmail.com>
2015-10-15 10:20:52 +0530

    UI Fixes in Grid Layout of Note

--- app/src/main/java/com/tilak/adpters/OurNoteListAdapter.java
+++ app/src/main/java/com/tilak/adpters/OurNoteListAdapter.java
@@ -2,15 +2,16 @@ package com.tilak.adpters;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.AbsListView.LayoutParams;
 import android.widget.TextView;
-import android.view.View;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
-import com.tilak.noteshare.R;
 
+import com.tilak.noteshare.R;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -50,6 +51,7 @@ public class OurNoteListAdapter extends BaseAdapter {
         TextView txtNoteName,txtNoteDesc,txtNoteDate, tvIdHidden;
         ImageButton btnLock, btnTimebomb, btnMove, btnDelete ,btnShare;
         LinearLayout linearLayout;
+        LinearLayout main;
     }
 
     @Override
@@ -70,12 +72,23 @@ public class OurNoteListAdapter extends BaseAdapter {
             holder.txtNoteDesc = (TextView) convertView.findViewById(R.id.tvNoteDesc);
             holder.txtNoteDate = (TextView) convertView.findViewById(R.id.tvNoteDate);
 
+            holder.main = (LinearLayout) convertView.findViewById(R.id.noteMain);
 
             if(listview == "list") {
+                holder.txtNoteName.setHeight(50);
                 holder.txtNoteName.setTextSize(18F);
                 holder.txtNoteDesc.setVisibility(View.GONE);
                 holder.txtNoteDate.setVisibility(View.GONE);
-                holder.linearLayout.setPadding(20,30,20,20);
+                holder.linearLayout.setPadding(20, 30, 20, 20);
+
+            }
+            if(listview == "detail"){
+                //holder.txtNoteName.setHeight(50);
+                /*FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
+                        FrameLayout.LayoutParams.WRAP_CONTENT);*/
+
+                LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, 125);
+                holder.main.setLayoutParams(params);
             }
 
             if(listview != "grid") {
@@ -86,6 +99,7 @@ public class OurNoteListAdapter extends BaseAdapter {
                 holder.btnMove = (ImageButton) convertView.findViewById(R.id.btnmove);
                 holder.btnDelete = (ImageButton) convertView.findViewById(R.id.btndelete);
                 holder.btnShare = (ImageButton) convertView.findViewById(R.id.btnshare);
+
             }
             convertView.setTag(holder);
         }else{
@@ -99,21 +113,22 @@ public class OurNoteListAdapter extends BaseAdapter {
         holder.txtNoteDate.setText(dbToAdapterDate(map.get("noteDate")));
 
         holder.linearLayout.setBackgroundColor(Color.parseColor(map.get("noteBgColor")));
-        if(listview == "grid") {
 
-            if(Integer.parseInt(map.get("noteNum")) % 2 != 0){
-                //holder.linearLayout.setBackgroundColor(Color.parseColor("#000000"));
-                /*LayoutParams params = new LayoutParams(
-                        LayoutParams.MATCH_PARENT,
-                        LayoutParams.WRAP_CONTENT);
+        if(listview == "grid"){
 
-                params.setMargins(0, 0, 5, 0);
-                holder.linearLayout.setLayoutParams((params));*/
-            }
+            /*if(Integer.parseInt(map.get("noteNum")) % 2 == 0){
+            LinearLayout.LayoutParams params = new
+                    LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT
+            );
+            params.setMargins(0,0,5,0);
+            holder.linearLayout.setLayoutParams(params);
+            }*/
         }
 
         if(map.get("noteLock").equalsIgnoreCase("1") && listview!="grid"){
-            holder.btnLock.setImageResource(R.drawable.image_option_lock2);
+            holder.btnLock.setImageResource(R.drawable.image_option_unlock);
         }
 
         if(listview != "grid") {
@@ -126,7 +141,6 @@ public class OurNoteListAdapter extends BaseAdapter {
             holder.btnShare.setTag(map.get("noteId"));
 
         }
-
         return convertView;
     }
 
@@ -145,129 +159,3 @@ public class OurNoteListAdapter extends BaseAdapter {
         return date;
     }
 }
-
-/*import android.app.Activity;
-import android.graphics.Color;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import com.tilak.noteshare.R;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class OurNoteListAdapter extends BaseAdapter {
-    public ArrayList<HashMap<String, String>> list;
-    String listview = "";
-    Activity activity;
-    TextView noteDesc;
-
-    public OurNoteListAdapter(Activity activity, ArrayList<HashMap<String, String>> list, String listwiew) {
-        super();
-        this.activity = activity;
-        this.list = list;
-        this.listview = listwiew;
-    }
-
-    @Override
-    public int getCount() {
-        return list.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return list.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    private class ViewHolder {
-        //all the fields in layout specified
-        TextView txtNoteName, txtNoteDesc, txtNoteDate, tvIdHidden;
-        ImageButton btnLock, btnTimebomb, btnMove, btnDelete, btnShare;
-        LinearLayout linearLayout;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        ViewHolder holder;
-
-        LayoutInflater inflater = activity.getLayoutInflater();
-        if (convertView == null) {
-            holder = new ViewHolder();
-            convertView = inflater.inflate(R.layout.our_note_list, null); //change the name of the layout
-            if (listview == "grid") {
-                convertView = inflater.inflate(R.layout.our_note_list_grid, null); //change the name of the layout
-            }
-
-            holder.linearLayout = (LinearLayout) convertView.findViewById(R.id.front);
-            holder.txtNoteName = (TextView) convertView.findViewById(R.id.tvNoteName); //find the different Views
-            holder.txtNoteDesc = (TextView) convertView.findViewById(R.id.tvNoteDesc);
-            holder.txtNoteDate = (TextView) convertView.findViewById(R.id.tvNoteDate);
-
-
-            if (listview == "list") {
-                holder.txtNoteName.setTextSize(18F);
-                holder.txtNoteDesc.setVisibility(View.GONE);
-                holder.txtNoteDate.setVisibility(View.GONE);
-                holder.linearLayout.setPadding(20, 30, 20, 20);
-            }
-
-            if (listview != "grid") {
-                holder.tvIdHidden = (TextView) convertView.findViewById(R.id.tvIdHidden);
-
-                holder.btnLock = (ImageButton) convertView.findViewById(R.id.btnlock);
-                holder.btnTimebomb = (ImageButton) convertView.findViewById(R.id.btntimebomb);
-                holder.btnMove = (ImageButton) convertView.findViewById(R.id.btnmove);
-                holder.btnDelete = (ImageButton) convertView.findViewById(R.id.btndelete);
-                holder.btnShare = (ImageButton) convertView.findViewById(R.id.btnshare);
-            }
-            convertView.setTag(holder);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
-        }
-
-        HashMap<String, String> map = list.get(position);
-        holder.txtNoteName.setText(map.get("noteName")); //set the hash maps
-        holder.txtNoteDesc.setText(map.get("noteDesc"));
-//        holder.txtNoteDate.setText(cdf.dbToAdapterDate(map.get("noteDate")));
-        holder.linearLayout.setBackgroundColor(Color.parseColor(map.get("noteBgColor")));
-        if (listview == "grid") {
-            if (Integer.parseInt(map.get("noteNum")) % 2 == 0) {
-                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                        LinearLayout.LayoutParams.MATCH_PARENT,
-                        LinearLayout.LayoutParams.WRAP_CONTENT
-                );
-                params.setMargins(0, 0, 5, 0);
-                holder.linearLayout.setLayoutParams(params);
-//                .setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-//                        LinearLayout.LayoutParams.MATCH_PARENT).setMargins(0, 0, 5, 0));
-            }
-
-        }
-
-        if (map.get("noteLock").equalsIgnoreCase("1") && listview != "grid") {
-            holder.btnLock.setImageResource(R.drawable.image_option_lock2);
-        }
-
-        if (listview != "grid") {
-            holder.tvIdHidden.setText(map.get("noteId"));
-            holder.btnLock.setTag(map.get("noteId"));
-            holder.btnTimebomb.setTag(map.get("noteId"));
-            holder.btnMove.setTag(map.get("noteId"));
-            holder.btnDelete.setTag(map.get("noteId"));
-            holder.btnShare.setTag(map.get("noteId"));
-        }
-
-        return convertView;
-    }
-}*/
