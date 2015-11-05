470e8332827e94ee5625d97c9a490a304e4949fa
Harsh Thakkar <harshthakkar46@gmail.com>
2015-11-05 19:06:54 +0530

    Facebook login done

--- app/src/main/java/com/tilak/noteshare/LoginActivity.java
+++ app/src/main/java/com/tilak/noteshare/LoginActivity.java
@@ -2,16 +2,89 @@ package com.tilak.noteshare;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.LoggingBehavior;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
 
 public class LoginActivity extends Activity {
+
+    LoginButton loginButton;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.login_activity);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("public_profile, email");
+        loginButton.registerCallback(callbackManager, facebookCallback);
+    }
 
+    FacebookCallback<LoginResult> facebookCallback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            GraphRequest request = GraphRequest.newMeRequest(
+                    loginResult.getAccessToken(),
+                    new GraphRequest.GraphJSONObjectCallback() {
+                        @Override
+                        public void onCompleted(JSONObject object, GraphResponse response) {
+
+                            if (BuildConfig.DEBUG) {
+                                FacebookSdk.setIsDebugEnabled(true);
+                                FacebookSdk.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
+
+                                Log.e("Harsh: AccessToken", AccessToken.getCurrentAccessToken().toString());
+                                Profile profile = Profile.getCurrentProfile();
+                                if (profile != null) {
+                                    String firstName = profile.getFirstName();
+                                    String lastName = profile.getLastName();
+                                    Uri pictureUri = profile.getProfilePictureUri(500, 500);
+                                    String email = object.optString("email");
+                                    String uid = object.optString("id");
+                                    Toast.makeText(getApplicationContext(), uid + " " + firstName + " " + lastName + " " + email + " " + pictureUri.toString(), Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(getApplication(), "Profile is null", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        }
+                    });
+            Bundle parameters = new Bundle();
+            parameters.putString("fields", "email");
+            request.setParameters(parameters);
+            request.executeAsync();
+        }
+
+        @Override
+        public void onCancel() {
+        }
+
+        @Override
+        public void onError(FacebookException e) {
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     public void main(View v){
