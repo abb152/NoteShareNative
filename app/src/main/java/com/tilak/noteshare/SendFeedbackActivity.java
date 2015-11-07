4f3f4e2be136d4a58ddd39e4f05563d9eb23c81f
jayvisariya93 <jayvisariya93@gmail.com>
2015-11-07 17:44:24 +0530

    Feedback partially done

--- app/src/main/java/com/tilak/noteshare/SendFeedbackActivity.java
+++ app/src/main/java/com/tilak/noteshare/SendFeedbackActivity.java
@@ -2,63 +2,127 @@ package com.tilak.noteshare;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class SendFeedbackActivity extends DrawerActivity {public RelativeLayout layoutHeder;
-public ImageButton btnheaderMenu,btnsequence,btncalander;
-public TextView textheadertitle,textViewSubHeaderTitle;
-public LinearLayout layoutTitleHeaderview;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class SendFeedbackActivity extends DrawerActivity {
+	public RelativeLayout layoutHeder;
+	public ImageButton btnheaderMenu, btnsequence, btncalander;
+	public TextView textheadertitle, textViewSubHeaderTitle;
+	public EditText textViewFeedbackText;
+	public LinearLayout layoutTitleHeaderview;
+	public Button btSubmit;
 
 
-protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	LayoutInflater inflater = (LayoutInflater) this
-			.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	// inflate your activity layout here!
-	View contentView = inflater
-			.inflate(R.layout.sendfeedback_activity, null, false);
-	mDrawerLayout.addView(contentView, 0);
-	initlizeUIElement(contentView);
-}
-void  initlizeUIElement(View contentView)
-{
-	//mainHeadermenue
-	layoutHeder=(RelativeLayout) contentView.findViewById(R.id.mainHeadermenue);
-	btnheaderMenu=(ImageButton) layoutHeder.findViewById(R.id.imageButtonHamburg);
-	
-	btnsequence=(ImageButton) layoutHeder.findViewById(R.id.imageButtonsquence);
-	btncalander=(ImageButton) layoutHeder.findViewById(R.id.imageButtoncalander);
-	btncalander.setVisibility(View.GONE);
-	btnsequence.setVisibility(View.GONE);
-	
-	///textheadertitle=(TextView) layoutHeder.findViewById(R.id.textViewheaderTitle);
-	//textheadertitle.setText("");
-	
-	
-	layoutTitleHeaderview=(LinearLayout) contentView.findViewById(R.id.titleHeaderview1);
-	textViewSubHeaderTitle=(TextView) layoutTitleHeaderview.findViewById(R.id.textViewHeaderTitle1);
-	textViewSubHeaderTitle.setText("Send Feedback");
-	
-	addListners();
-}
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		LayoutInflater inflater = (LayoutInflater) this
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		// inflate your activity layout here!
+		View contentView = inflater
+				.inflate(R.layout.sendfeedback_activity, null, false);
+		mDrawerLayout.addView(contentView, 0);
+		initlizeUIElement(contentView);
 
-@Override
-public void addListners() {
-	// TODO Auto-generated method stub
-	super.addListners();
-	btnheaderMenu.setOnClickListener(new OnClickListener() {
-		
-		@Override
-		public void onClick(View v) {
-			// TODO Auto-generated method stub
-			openSlideMenu();
+
+		if (android.os.Build.VERSION.SDK_INT > 9) {
+			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+			StrictMode.setThreadPolicy(policy);
 		}
-	});
-	
-}}
+	}
+
+	void initlizeUIElement(View contentView) {
+		//mainHeadermenue
+		layoutHeder = (RelativeLayout) contentView.findViewById(R.id.mainHeadermenue);
+		btnheaderMenu = (ImageButton) layoutHeder.findViewById(R.id.imageButtonHamburg);
+
+		btnsequence = (ImageButton) layoutHeder.findViewById(R.id.imageButtonsquence);
+		btncalander = (ImageButton) layoutHeder.findViewById(R.id.imageButtoncalander);
+		btncalander.setVisibility(View.GONE);
+		btnsequence.setVisibility(View.GONE);
+
+		textViewFeedbackText = (EditText) findViewById(R.id.etFeedback);
+
+
+		///textheadertitle=(TextView) layoutHeder.findViewById(R.id.textViewheaderTitle);
+		//textheadertitle.setText("");
+
+
+		layoutTitleHeaderview = (LinearLayout) contentView.findViewById(R.id.titleHeaderview1);
+		textViewSubHeaderTitle = (TextView) layoutTitleHeaderview.findViewById(R.id.textViewHeaderTitle1);
+		textViewSubHeaderTitle.setText("Send Feedback");
+
+		btSubmit = (Button) findViewById(R.id.btSubmit);
+
+		addListners();
+	}
+
+	@Override
+	public void addListners() {
+		// TODO Auto-generated method stub
+		super.addListners();
+		btnheaderMenu.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				openSlideMenu();
+			}
+		});
+		btSubmit.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				try {
+					sendFeedback();
+				} catch (JSONException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+
+	public void sendFeedback() throws JSONException, ClientProtocolException, IOException {
+
+		Log.e("jay in getServerData","");
+		ArrayList<String> stringData = new ArrayList<String>();
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+		ResponseHandler<String> resonseHandler = new BasicResponseHandler();
+		HttpPost postMethod = new HttpPost("http://104.197.122.116/feed/save");
+
+		JSONObject json = new JSONObject();
+		json.put("user", "56120af8a89c4c8f043a0285");
+		json.put("email", "heyMogambo@wohlig.com");
+		json.put("text", "Mogambo Khush hua");
+		//postMethod.setHeader("Content-Type", "application/json" );
+		postMethod.setEntity(new ByteArrayEntity(json.toString().getBytes("UTF8")));
+		String response = httpClient.execute(postMethod,resonseHandler);
+		Log.e("jay response :", response);
+		JSONObject responseJson = new JSONObject(response);
+		String value = responseJson.get("value").toString();
+
+	}
+}
